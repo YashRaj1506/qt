287f66fe5985186bd460e0baa506d41d6c208404
YashRaj1506 <yashraj504300@gmail.com>
2025-02-19 23:34:53 +0530

    handled layouts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, 
+                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
@@ -11,6 +12,40 @@ class MainWindow(QMainWindow):
         self.setGeometry(700,300, 500, 500)
         self.setWindowIcon(QIcon("1.png"))
         # self.setFixedSize(500,500)
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
+
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
+
+        # vbox = QVBoxLayout()
+
+        # vbox.addWidget(label1, 0,0)
+        # vbox.addWidget(label2, 0, 1)
+        # vbox.addWidget(label3, 1, 0)
+        # vbox.addWidget(label4, 1, 1)
+        # vbox.addWidget(label5,1,2)
+        vbox = QGridLayout()
+
+        vbox.addWidget(label1, 0,0)
+        vbox.addWidget(label2, 0, 1)
+        vbox.addWidget(label3, 1, 0)
+        vbox.addWidget(label4, 1, 1)
+        vbox.addWidget(label5,1,2)
+
+        central_widget.setLayout(vbox)
 
 
 #       ------------labels align------------
